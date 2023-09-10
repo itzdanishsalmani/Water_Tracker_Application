e24464ffafb7e4cb267a4ee85678acf009af3fb8
CoderDanish488 <salmanidanish488@gmail.com>
2023-09-10 11:21:22 +0530

    database

--- app/src/main/java/com/example/water_traker_application/MainActivity3.kt
+++ app/src/main/java/com/example/water_traker_application/MainActivity3.kt
@@ -14,6 +14,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -38,6 +41,11 @@ class MainActivity3 : AppCompatActivity() {
 
     // SharedPreferences
     private lateinit var sharedPreferences: SharedPreferences
+
+    // Firebase
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private var userEmail: String? = null // User's Gmail address
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -86,11 +94,24 @@ class MainActivity3 : AppCompatActivity() {
             val intent44 = Intent(this, MainActivity5::class.java)
             startActivity(intent44)
         }
-    }
 
-    override fun onBackPressed() {
-        // Finish MainActivity3, removing it from the back stack
-        finish()
+        // Initialize Firebase
+        firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        // Configure Firestore settings for offline persistence
+        configureFirestoreOfflinePersistence()
+
+        // Check if the user is authenticated
+        if (firebaseAuth.currentUser != null) {
+            userEmail = firebaseAuth.currentUser?.email
+        }
+
+        // Fetch requiredML from Firestore
+        fetchRequiredMLFromFirestore()
+
+        // Restore currentML from SharedPreferences
+        restoreCurrentMLFromSharedPreferences()
     }
 
     private fun addWater(mlToAdd: Float) {
@@ -106,22 +127,25 @@ class MainActivity3 : AppCompatActivity() {
             // Log water in the background
             logWaterInBackground(mlToAdd)
 
-            // Inside the addWater() function, after updating the currentML
-            if (currentML >= requiredML) {
-                val remainingML = requiredML - currentML
-                val intent = Intent(this@MainActivity3, MainActivity5::class.java)
-                intent.putExtra("isGoalCompleted", true)
-                intent.putExtra("remainingML", remainingML)
-                startActivity(intent)
-            }
+            // Save currentML to SharedPreferences
+            saveCurrentMLToSharedPreferences(currentML)
+
+            // Update Firestore data
+            updateUserFirestoreData()
         }
     }
 
     private suspend fun logWaterInBackground(mlAdded: Float) = withContext(Dispatchers.Default) {
         val currentTime = getCurrentTime()
+        val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+
+        // Create a WaterLog object
         val log = WaterLog(currentTime, mlAdded)
+
+        // Add the log to the RecyclerView
         waterLogs.add(log)
 
+        // Notify the RecyclerView adapter
         withContext(Dispatchers.Main) {
             logAdapter.notifyDataSetChanged()
         }
@@ -143,5 +167,69 @@ class MainActivity3 : AppCompatActivity() {
             logAdapter.clearData()
             previousDate = currentDate
         }
+    }
+
+    private fun updateUserFirestoreData() {
+        userEmail?.let { email ->
+            // Define the data to update in Firestore
+            val data = hashMapOf<String, Any>(
+                "currentML" to currentML,
+                "requiredML" to requiredML
+            )
+
+            // Update Firestore with the user's Gmail address as the document ID
+            firestore.collection("users")
+                .document(email)
+                .update(data) // Use update() instead of set() to update specific fields
+                .addOnSuccessListener {
+                    // Data updated successfully in Firestore
+                }
+                .addOnFailureListener { e ->
+                    // Handle errors here
+                }
+        }
+    }
+
+    private fun fetchRequiredMLFromFirestore() {
+        userEmail?.let { email ->
+            firestore.collection("users")
+                .document(email)
+                .get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val requiredMLFromFirestore = documentSnapshot.getDouble("requiredML")
+                        if (requiredMLFromFirestore != null) {
+                            requiredML = requiredMLFromFirestore.toFloat()
+                            requiredMLTextView.text = String.format("%.0f ML", requiredML)
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Handle errors here
+                }
+        }
+    }
+
+    private fun saveCurrentMLToSharedPreferences(currentML: Float) {
+        val editor = sharedPreferences.edit()
+        editor.putFloat("currentML", currentML)
+        editor.apply()
+    }
+
+    private fun restoreCurrentMLFromSharedPreferences() {
+        currentML = sharedPreferences.getFloat("currentML", 0f)
+        currentMLTextView.text = String.format("%.0f ML", currentML)
+
+        // Update the circularProgressBar as well
+        val progressPercentage = (currentML / requiredML) * 100
+        circularProgressBar.setProgressWithAnimation(progressPercentage, 500)
+    }
+
+    private fun configureFirestoreOfflinePersistence() {
+        // Enable Firestore offline persistence
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build()
+        firestore.firestoreSettings = settings
     }
 }
