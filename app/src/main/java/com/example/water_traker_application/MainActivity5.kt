42d2d258f1960e8a4268d60c343a89a1dfe0254e
Danish <salmanidanish488@gmail.com>
2023-09-28 10:24:35 +0530

    weekly & monthly average

--- app/src/main/java/com/example/water_traker_application/MainActivity5.kt
+++ app/src/main/java/com/example/water_traker_application/MainActivity5.kt
@@ -6,9 +6,11 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
@@ -16,11 +18,12 @@ class MainActivity5 : AppCompatActivity() {
 
     private lateinit var calendarView: CalendarView
     private lateinit var percentageTextView: TextView
+    private lateinit var weeklyAverageData: TextView
+    private lateinit var monthlyAverageData: TextView
 
-    // Firebase
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
-    private var userEmail: String? = null // User's Gmail address
+    private var userEmail: String? = null
     private val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,37 +32,27 @@ class MainActivity5 : AppCompatActivity() {
 
         calendarView = findViewById(R.id.calendarView)
         percentageTextView = findViewById(R.id.percentageTextView)
+        weeklyAverageData = findViewById(R.id.weeklyAverageData)
+        monthlyAverageData = findViewById(R.id.monthlyAverageData)
 
-        // Initialize Firebase
         firebaseAuth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
-
-        // Configure Firestore settings for offline persistence
         configureFirestoreOfflinePersistence()
 
-        // Check if the user is authenticated
         if (firebaseAuth.currentUser != null) {
             userEmail = firebaseAuth.currentUser?.email
         }
 
-        // Set the maximum date for the CalendarView to the current date
         calendarView.maxDate = Date().time
 
-        // Set up the OnDateChangeListener for the CalendarView
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             val selectedDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
                 .format(Date(year - 1900, month, dayOfMonth))
 
-            // Check if the selected date is not in the future
             if (selectedDate <= currentDate) {
-                // Fetch data from Firestore for the selected date
                 fetchFirestoreData(selectedDate)
             } else {
-                // Selected date is in the future, display a message or take appropriate action
-                // For example, you can show a Toast message indicating that future dates cannot be selected
-
-                    Toast.makeText(this,"Future date cannot be selected", Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(this, "Future date cannot be selected", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -75,7 +68,6 @@ class MainActivity5 : AppCompatActivity() {
                         val currentML = documentSnapshot.getDouble("currentML") ?: 0.0
                         val requiredML = documentSnapshot.getDouble("requiredML") ?: 0.0
 
-                        // Calculate and display the percentage
                         val percentage = if (requiredML > 0) {
                             val calculatedPercentage = (currentML / requiredML * 100).toInt()
                             if (calculatedPercentage > 100) {
@@ -88,23 +80,139 @@ class MainActivity5 : AppCompatActivity() {
                         }
 
                         percentageTextView.text = "$percentage% completed"
+
+                        calculateAndDisplayWeeklyAverage(selectedDate)
+                        calculateAndDisplayMonthlyAverage()
                     } else {
-                        // Data not found for the selected date, display 0%
                         percentageTextView.text = "0%"
                     }
                 }
                 .addOnFailureListener { e ->
-                    // Handle errors here
                     percentageTextView.text = "Refresh"
                 }
         }
     }
 
+    private fun calculateAndDisplayMonthlyAverage() {
+        userEmail?.let { email ->
+            val calendar = Calendar.getInstance()
+            calendar.time = Date()
+            calendar.set(Calendar.DAY_OF_MONTH, 1) // Set to the 1st day of the current month
+            val startDate =
+                SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(calendar.time)
+            val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+
+            firestore.collection("users/$email/dailyData")
+                .whereGreaterThanOrEqualTo(FieldPath.documentId(), startDate)
+                .whereLessThanOrEqualTo(FieldPath.documentId(), currentDate)
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    val dailyPercentages = mutableListOf<Double>()
+                    querySnapshot.documents.forEach { documentSnapshot ->
+                        val currentML = documentSnapshot.getDouble("currentML") ?: 0.0
+                        val requiredML = documentSnapshot.getDouble("requiredML") ?: 0.0
+
+                        val dailyPercentage = if (requiredML > 0) {
+                            val calculatedPercentage = (currentML / requiredML * 100).toInt()
+                            if (calculatedPercentage > 100) {
+                                100.0
+                            } else {
+                                calculatedPercentage.toDouble()
+                            }
+                        } else {
+                            0.0
+                        }
+
+                        dailyPercentages.add(dailyPercentage)
+                    }
+
+                    if (dailyPercentages.isNotEmpty()) {
+                        val monthlyAverage = dailyPercentages.average().toInt()
+                        monthlyAverageData.text = "Monthly Average: $monthlyAverage%"
+                    } else {
+                        monthlyAverageData.text = "Monthly Average: 0%"
+                    }
+                }
+                .addOnFailureListener { e ->
+                    monthlyAverageData.text = "Monthly Average: Error"
+                }
+        }
+    }
+
     private fun configureFirestoreOfflinePersistence() {
-        // Enable Firestore offline persistence
         val settings = FirebaseFirestoreSettings.Builder()
             .setPersistenceEnabled(true)
             .build()
         firestore.firestoreSettings = settings
     }
+
+    private fun calculateAndDisplayWeeklyAverage(selectedDate: String) {
+        userEmail?.let { email ->
+            val startDate = getStartOfWeek(selectedDate)
+            val endDate = getEndOfWeek(selectedDate)
+
+            firestore.collection("users/$email/dailyData")
+                .whereGreaterThanOrEqualTo(FieldPath.documentId(), startDate)
+                .whereLessThanOrEqualTo(FieldPath.documentId(), endDate)
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    val dailyPercentages = mutableListOf<Int>() // Use Int instead of Double
+                    querySnapshot.documents.forEach { documentSnapshot ->
+                        val currentML = documentSnapshot.getDouble("currentML") ?: 0.0
+                        val requiredML = documentSnapshot.getDouble("requiredML") ?: 0.0
+
+                        val dailyPercentage = if (requiredML > 0) {
+                            val calculatedPercentage = (currentML / requiredML * 100).toInt()
+                            if (calculatedPercentage > 100) {
+                                100 // Cap the daily percentage at 100
+                            } else {
+                                calculatedPercentage
+                            }
+                        } else {
+                            0
+                        }
+
+                        dailyPercentages.add(dailyPercentage)
+                    }
+
+                    if (dailyPercentages.isNotEmpty()) {
+                        val weeklySum = dailyPercentages.sum()
+                        val weeklyAverage = weeklySum / 7 // Divide by 7 to get a consistent weekly average
+                        weeklyAverageData.text = "Weekly Average: ${weeklyAverage}%"
+                    } else {
+                        weeklyAverageData.text = "Weekly Average: 0%"
+                    }
+                }
+                .addOnFailureListener { e ->
+                    weeklyAverageData.text = "Weekly Average: Error"
+                }
+        }
+    }
+
+    private fun getStartOfWeek(date: String): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val parsedDate = dateFormat.parse(date)
+        val calendar = Calendar.getInstance()
+        calendar.time = parsedDate
+
+        // Set the start day of the week to Monday
+        calendar.firstDayOfWeek = Calendar.MONDAY
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+
+        return dateFormat.format(calendar.time)
+    }
+
+    private fun getEndOfWeek(date: String): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val parsedDate = dateFormat.parse(date)
+        val calendar = Calendar.getInstance()
+        calendar.time = parsedDate
+
+        // Set the start day of the week to Monday
+        calendar.firstDayOfWeek = Calendar.MONDAY
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY)
+
+        return dateFormat.format(calendar.time)
+    }
+
 }
